15c596c591cd8b7d252166047736e0cfe8dd05a8
jaco <jacodesouza113@gmail.com>
2022-02-25 22:45:29 -0300

    first commit

---
+++ senha.js
@@ -0,0 +1,14 @@
+var min = 0
+var med = 500
+var max = 1000
+
+function rand({ min = 0, med = 500, max = 1000 }) { 
+    const valor = Math.random() * (max - med - min) + min
+     return Math.floor(valor)
+}
+(min - med - max)
+
+const obj = { min: 0, med: 500, max: 1000}
+console.log(rand(obj))
+console.log(rand({ min: 955}))
+console.log(rand({max: 1000}))
